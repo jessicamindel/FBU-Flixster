0cb6ab2677df09c688b2c90cd6b2908f07744e7c
Jessie Mindel <mindel.jessie@gmail.com>
2019-06-27 12:28:51 -0700

    Add MovieAdapter, movie layout

---
+++ app/src/main/java/com/jmindel/flixster/MovieAdapter.java
@@ -0,0 +1,84 @@
+package com.jmindel.flixster;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jmindel.flixster.models.Movie;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivPosterImage;
+        TextView tvTitle, tvOverview;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ivPosterImage = itemView.findViewById(R.id.ivPosterImage);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+        }
+    }
+
+    ArrayList<Movie> movies;
+
+    public MovieAdapter(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
+    /**
+     * Creates and inflates a new view.
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Get the context and the inflater
+        // LEARN: How are contexts used, and what do they signify?
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        // Inflate the movie view into the parent => create the view based on the layout file
+        // LEARN: Which root would we be attaching to? The XML root, or something else? And when/why would we want to do that?
+        // LEARN: How is a context related to a ViewGroup? Is everything a ViewGroup?
+        View movieView = inflater.inflate(R.layout.item_movie, parent, false);
+        // Return a new ViewHolder that contains the new view
+        /* LEARN: What's the point of the ViewHolder? It sounds like it's just for accessing different parts of the view easily,
+         *  but why is it standardized in the polymorphic hierarchy? How could it be a pattern if there doesn't seem like there's
+         *  much else to store other than what the immediate consumer wants, and no business logic behind it?
+         */
+        return new ViewHolder(movieView);
+    }
+
+    /**
+     * Binds an inflated view to a new item.
+     * @param holder
+     * @param position
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Fetch the selected movie
+        Movie movie = movies.get(position);
+        // Load data into ViewHolder
+        holder.tvTitle.setText(movie.getTitle());
+        holder.tvOverview.setText(movie.getOverview());
+        // TODO: Set image using Glide
+    }
+
+    /**
+     * Returns the total number of items in the list.
+     * @return
+     */
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+}
--- app/src/main/java/com/jmindel/flixster/MovieListActivity.java
+++ app/src/main/java/com/jmindel/flixster/MovieListActivity.java
@@ -6,6 +6,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.jmindel.flixster.models.Movie;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -13,6 +14,8 @@ import com.loopj.android.http.RequestParams;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -24,19 +27,47 @@ public class MovieListActivity extends AppCompatActivity {
 
     AsyncHttpClient client;
     String imageBaseUrl, posterSize;
+    ArrayList<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
         client = new AsyncHttpClient();
+        movies = new ArrayList<>();
         getConfiguration();
+    }
+
+    private void getNowPlaying() {
+        String url = API_BASE_URL + "/movie/now_playing";
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key));
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONArray results = response.getJSONArray("results");
+                    for (int i = 0; i < results.length(); i++) {
+                        Movie m = new Movie(results.getJSONObject(i));
+                        movies.add(m);
+                    }
+                    Log.i(TAG, String.format("Loaded %d movies", results.length()));
+                } catch (JSONException e) {
+                    logError("Failed to parse now_playing response", e, true);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                logError("Failed to get data from now_playing endpoint", throwable, true);
+            }
+        });
     }
 
     private void getConfiguration() {
         String url = API_BASE_URL + "/configuration";
         RequestParams params = new RequestParams();
-        params.put(API_KEY_PARAM, R.string.api_key);
+        params.put(API_KEY_PARAM, getString(R.string.api_key));
         client.get(url, params, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
@@ -45,6 +76,9 @@ public class MovieListActivity extends AppCompatActivity {
                     imageBaseUrl = images.getString("secure_base_url");
                     JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
                     posterSize = posterSizeOptions.optString(4, "w342");
+                    Log.i(TAG, String.format("Loaded configuration with imageBaseUrl %s and posterSize %s", imageBaseUrl, posterSize));
+                    // FIXME: Temporary until fully asynchronous, I think...
+                    getNowPlaying();
                 } catch (JSONException e) {
                     logError("Secure base url not found", e, true);
                 }
@@ -52,7 +86,7 @@ public class MovieListActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                logError("Failed getting configuration", throwable, true);
+                logError("Failed to get configuration", throwable, true);
             }
         });
     }
